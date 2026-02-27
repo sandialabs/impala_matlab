9fc7947b730f32269394a16017391840588727e9
Derek Tucker <jdtuck@sandia.gov>
2026-02-27 15:04:27 -0500

    add plims

---
+++ plims.m
@@ -0,0 +1,9 @@
+function y=plims(x,p)
+%PLIMS Empirical quantiles
+% plims(x,p)  calculates p quantiles from columns of x
+if nargin<2
+    %  p = [0.025,0.975];
+    p = [0.25,0.5,0.75];
+end
+[n,m] = size(x); if n==1; n=m;end
+y = interp1(sort(x),(n-1)*p+1);
